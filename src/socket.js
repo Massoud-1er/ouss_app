bafa2cdac012c229f8a0edf7be951aad3dfd1833
massoud <monsieurmassoud@gmail.com>
2020-02-28 18:22:19 +0100

    cadeau ouss

--- src/socket.js
+++ src/socket.js
@@ -13,11 +13,6 @@ wss.on('connection', (ws, req) => {
       if (users[req.session._id]) {
         users[req.session._id].send(message)
         console.log('message.sent')
-        // new Message({
-        //   // sender: req.session.device_id,
-        //   // match: req.session.match_id,
-        //   content: message
-        // }).save()
       }
     })
   })
